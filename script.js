16f7212da25a0fe01bc3541dbeacb873685c9d64
elix593 <0988000007vera@gmail.com>
2024-10-21 18:23:18 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulario enviado correctamente.');
+    // Aquí podrías agregar la lógica para enviar el formulario a un servidor.
+});
